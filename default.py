aa9d58bdc5c144ca96448a2ab3f94eeeeb77df6d
Marcin Myśliwiec <marcin.mysliw@gmail.com>
2020-06-26 08:54:33 +0200

    Code refactor

---
+++ default.py
@@ -0,0 +1,7 @@
+from pathlib import Path
+
+default = {
+    "SOURCE_FOLDER": Path('data/input'),
+    "DEST_FOLDER": Path('data/output'),
+    "FILENAME": Path('result.pdf'),
+}
--- main.py
+++ main.py
@@ -1,34 +1,42 @@
-import argparse
 import os
+import sys
+import argparse
+
+from default import default
+from pathlib import Path
 
 from PDF.Combine import PDFCombine
 from PDF.Watermark import PDFWatermark
 from PDF.ParseName import PDFParseName
 
-if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description='Merge directory filled with PDFs')
-
-    parser.add_argument('source_folder', help='Source folder with PDFs')
-    parser.add_argument('output_filename', help='Desired output filename')
-    parser.add_argument('--destination-folder', help='Default value=output.')
-    parser.add_argument('--watermark', help='Add watermark for your new pdf.')
-
-    # Take parameters from terminal command execution
-    args = parser.parse_args()
-    source_folder = args.source_folder
-    output_filename = args.output_filename
-    destination_folder = args.destination_folder if args.destination_folder else 'output'
+def main(args):
+    source_folder = Path(args.source_folder) if args.source_folder else default["SOURCE_FOLDER"]
+    destination_folder = Path(args.destination_folder) if args.destination_folder else default["DEST_FOLDER"]
+    output_filename = args.output_filename if args.output_filename else default["FILENAME"]
 
     # Create output folder if does not exists
-    if not os.path.exists(destination_folder):
-        os.makedirs(destination_folder)
+    if not Path(destination_folder).exists():
+        Path(destination_folder).mkdir()
+
 
     # Check if user provided valid output name
     output_filename = PDFParseName(output_filename)
-    destination_full_path = os.path.join(destination_folder, output_filename)
+    destination_full_path = str(Path(destination_folder, output_filename))
 
-    print('Saving file: ' + destination_full_path)
+    # print('Saving file: ' + str(destination_full_path))
     PDFCombine(source_folder, destination_full_path)
     if args.watermark:
         PDFWatermark(destination_full_path, args.watermark)
     print('File saved!')
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='Merge directory filled with PDFs')
+
+    parser.add_argument('--source-folder', help='Source folder with PDFs. Default ')
+    parser.add_argument('--destination-folder', help='Desired destination folder. Default ' + str(default["SOURCE_FOLDER"]))
+    parser.add_argument('--output-filename', help='Desired output filename. Default ' + str(default["FILENAME"]))
+    parser.add_argument('--watermark', help='Add watermark for your new pdf. No watermark if empty')
+
+    # Take parameters from terminal command execution
+    args = parser.parse_args()
+    sys.exit(main(args))
